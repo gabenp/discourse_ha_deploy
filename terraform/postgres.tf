dc7bbc71c303317b65872d26fd5e740d3ccb1c9d
xstaticxgpx <root@un1x.su>
2021-11-03 17:15:08 -0400

    Drop some DB related terraform in favor of manual configuration

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -6,22 +6,3 @@ resource "digitalocean_database_cluster" "discourse" {
   region     = "nyc1"
   node_count = 2
 }
-
-resource "digitalocean_database_db" "discourse_db" {
-  cluster_id = digitalocean_database_cluster.discourse.id
-  name       = "discourse"
-}
-
-resource "digitalocean_database_user" "discourse" {
-  cluster_id = digitalocean_database_cluster.discourse.id
-  name       = "discourse"
-}
-
-resource "digitalocean_database_connection_pool" "discourse-pool-01" {
-  cluster_id = digitalocean_database_cluster.discourse.id
-  name       = "discourse-pool-01"
-  mode       = "transaction"
-  size       = 20
-  db_name    = "discourse"
-  user       = "discourse"
-}
