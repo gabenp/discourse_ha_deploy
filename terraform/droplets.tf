a776ff6f39ea1dbbca77aabf55b43c55cf912d84
xstaticxgpx <root@un1x.su>
2021-11-03 19:29:29 -0400

    Droplets get templated user_data for bootstrap

--- terraform/droplets.tf
+++ terraform/droplets.tf
@@ -5,11 +5,19 @@ resource "digitalocean_ssh_key" "root" {
 }
 
 resource "digitalocean_droplet" "discourse_ha" {
-  count     = 2
-  image     = "ubuntu-20-04-x64"
-  name      = "discourse-${count.index}"
-  region    = "nyc1"
-  size      = "s-1vcpu-1gb"
-  user_data = file("./droplet_user_data.txt")
-  ssh_keys  = [digitalocean_ssh_key.root.id]
+  count    = 2
+  image    = "ubuntu-20-04-x64"
+  name     = "discourse-${count.index}"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.root.id]
+  user_data = templatefile(
+    "./templates/droplet_user_data.tpl",
+    {
+      doadmin_defaultdb_uri = digitalocean_database_cluster.discourse.uri
+      discourse_db_host     = digitalocean_database_cluster.discourse.host
+      discourse_db_port     = digitalocean_database_cluster.discourse.port
+      discourse_db_user     = digitalocean_database_user.discourse.name
+      discourse_db_pass     = digitalocean_database_user.discourse.password
+  })
 }
